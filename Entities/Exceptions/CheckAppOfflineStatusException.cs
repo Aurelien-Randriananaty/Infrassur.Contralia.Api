97b369256c3bf4f2df70ee6b85105b42852153c9
Joro Aurélien Randriananaty <randriananaty@gmail.com>
2023-06-09 09:20:46 +0300

    Wip ecxeption Handler

---
+++ Entities/Exceptions/CheckAppOfflineStatusException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+	public class CheckAppOfflineStatusException : BadRequestException
+	{
+		public CheckAppOfflineStatusException() : base("API is not accessible, app is offline")
+		{
+		}
+	}
+}
